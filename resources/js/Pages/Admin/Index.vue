665b1241665b23aba8fbbefa2c415f33e62543a7
Satendra Kumar <kumarsd.aln@gmail.com>
2026-04-22 11:21:31 +0530

    delete Pages/Admin/Index

--- resources/js/Pages/Admin/Index.vue
+++
@@ -1,9 +0,0 @@
-<!--
-This monolithic Admin/Index.vue file has been deprecated and safely replaced by dedicated components:
-- Admin/Dashboard.vue
-- Admin/Users.vue
-- Admin/Plans.vue
-- Admin/Subscriptions.vue
-
-This file can be safely deleted from version control.
--->
